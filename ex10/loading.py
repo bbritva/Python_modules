7708361d48e08fe9d81a0897b9781649f358e21e
grvelva <grvelva@student.42.fr>
2023-03-29 18:32:34 +0200

    some correction

--- ex10/loading.py
+++ ex10/loading.py
@@ -3,26 +3,26 @@ import time
 def ft_progress(list):
     start = time.time()
     max_value = max(list)
-    elapsed = 0
+    eta = 0
     for i in list:
         now = time.time() - start
         percent = 100 * (i) / max_value
         if percent > 0:
-            elapsed = (100 - percent) * now / percent
+            eta = 100 * now / percent
         filled = int(percent / 5)
         if filled == 20:
             empty = 0
         else:
             empty = 19 - filled
-        line = "ETA: %5.2fs [%3d%%]" % (now, percent)
+        line = "ETA: %5.2fs [%3d%%]" % (eta, percent)
         line += "[" + '=' * filled +'>' * (filled != 20) + ' ' * empty + ']'
-        line += "%4d/%4d | elapsed time %5.2fs" % (i + 1, max_value + 1, elapsed)
+        line += "%4d/%4d | elapsed time %5.2fs" % (i + 1, max_value + 1, now)
         print(line, end="\r")
         yield i
 
 "====================>"
 if __name__ == "__main__":
-    listy = range(3333)
+    listy = range(1000)
     ret = 0
     for elem in ft_progress(listy):
         ret += (elem + 3) % 5
