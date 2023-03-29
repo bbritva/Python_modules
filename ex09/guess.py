5caded716ae8d2b7cf7ae955f627ba723f35c5e1
grvelva <grvelva@student.42.fr>
2023-03-29 17:00:17 +0200

    ex09

---
+++ ex09/guess.py
@@ -0,0 +1,35 @@
+import random
+
+if __name__ == "__main__":
+    print("""This is an interactive guessing game!
+You have to enter a number between 1 and 99 to find out the secret number.
+Type 'exit' to end the game.
+Good luck!""")
+    num = random.randint(1, 99)
+    num = 42
+    attempts = 0
+    while True:
+        attempts += 1
+        try:
+            line = input("What's your guess between 1 and 99\n")
+            if line == 'exit':
+                print("Goodbye!")
+                break
+            inp_num = int(line)
+            if inp_num > num:
+                print("Too high!")
+            elif inp_num < num:
+                print("Too low!")
+            else:
+                if (num == 42):
+                    print("The answer to the ultimate question of life, the universe and everything is 42.")
+                if (attempts == 1):
+                    print("Congratulations! You got it on your first try!")
+                else:
+                    print("Congratulations, you've got it!")
+                    print("You won in {} attempts!".format(attempts))
+                break
+        except ValueError:
+            print("That's not a number.")
+
+        
