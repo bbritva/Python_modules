c17430ab800c30a26dc15a893fc3d554a0bc073c
grvelva <grvelva@student.42.fr>
2023-03-30 16:57:23 +0200

    proyected from tricky cases

--- ex10/loading.py
+++ ex10/loading.py
@@ -3,11 +3,17 @@ import time
 
 def ft_progress(list):
     start = time.time()
-    max_value = max(list)
+    start_value = list[0]
+    end_value = list[len(list) - 1]
+    diff = end_value - start_value
+    if diff == 0:
+        diff = 1
     eta = 0
     for i in list:
         now = time.time() - start
-        percent = 100 * (i) / max_value
+        percent = 100 * (i - start_value) / diff
+        if i == end_value:
+            percent = 100
         if percent > 0:
             eta = 100 * now / percent
         filled = int(percent / 5)
@@ -17,16 +23,16 @@ def ft_progress(list):
             empty = 19 - filled
         line = "ETA: %5.2fs [%3d%%] " % (eta, percent)
         line += "[" + '=' * filled + '>' * (filled != 20) + ' ' * empty + '] '
-        line += "%4d/%4d | elapsed time %5.2fs" % (i + 1, max_value + 1, now)
+        line += "%4d/%4d | elapsed time %5.2fs" % (i, end_value, now)
         print(line, end="\r")
         yield i
 
 
 if __name__ == "__main__":
-    listy = range(3333)
+    listy = range(100)
     ret = 0
     for elem in ft_progress(listy):
         ret += elem
-        time.sleep(0.005)
+        time.sleep(0.001)
     print()
     print(ret)
