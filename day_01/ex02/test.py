584d2baaece47c8a24a6cf05290a140d8af20fcf
grvelva <grvelva@student.42.fr>
2023-04-06 15:15:03 +0200

    added \'overloading\' in init

--- day_01/ex02/test.py
+++ day_01/ex02/test.py
@@ -2,6 +2,10 @@ from vector import Vector
 
 if __name__ == '__main__':
     vct = Vector([[1.], [2.], [3.]])
-    print(vct.shape)
+    print(vct)
     vct = Vector([[1., 2., 3.]])
-    print(vct.shape)
+    print(vct)
+    vct = Vector(3)
+    print(vct)
+    vct = Vector((10, 16))
+    print(vct)
--- day_01/ex02/vector.py
+++ day_01/ex02/vector.py
@@ -1,8 +1,21 @@
 class Vector:
     def __init__(self, values):
-        self.values = values
-        self.shape = (len(values), len(values[0]))
-    def __init__(self, values):
-        self.values = values
-        self.shape = (len(values), len(values[0]))
+        if isinstance(values, list):
+            self.values = values
+        elif isinstance(values, int):
+            self.values = []
+            innerList = []
+            for i in range(values):
+                innerList.append(float(i))
+            self.values.append(innerList)
+        elif isinstance(values, tuple):
+            self.values = []
+            innerList = []
+            for i in range(values[0], values[1]):
+                innerList.append(float(i))
+            self.values.append(innerList)
+        self.shape = (len(self.values), len(self.values[0]))
+
+    def __str__(self):
+        return self.values.__str__()
 
