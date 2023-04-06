b6196cecd4345a02ff8e51a565af96669534bac3
grvelva <grvelva@student.42.fr>
2023-04-06 17:30:44 +0200

    added constructor protection

--- day_01/ex02/vector.py
+++ day_01/ex02/vector.py
@@ -1,7 +1,17 @@
+def checkList(values):
+    return len(values) > 0 and isinstance(values[0], list) and \
+    len(values[0]) > 0 and isinstance(values[0][0], float)
+
+def checkShape(shape):
+    return (shape[0] == 1 and shape[1] > 0) or (shape[0] > 0 and shape[1] == 1) 
+
 class Vector:
     def __init__(self, values):
         if isinstance(values, list):
-            self.values = values
+            if checkList(values):
+                self.values = values
+            else:
+                raise ValueError
         elif isinstance(values, int):
             self.values = []
             innerList = []
@@ -9,12 +19,16 @@ class Vector:
                 innerList.append(float(i))
             self.values.append(innerList)
         elif isinstance(values, tuple):
+            if len(values) != 2 or values[0] > values[1]:
+                raise ValueError
             self.values = []
             innerList = []
             for i in range(values[0], values[1]):
                 innerList.append(float(i))
             self.values.append(innerList)
         self.shape = (len(self.values), len(self.values[0]))
+        if not checkShape(self.shape):
+            raise ValueError
 
     def __str__(self):
         return self.values.__str__()
