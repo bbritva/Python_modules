3d9fcbc266e37bfca112b0b6c417bcdc69ce3999
grvelva <grvelva@student.42.fr>
2023-04-06 14:27:55 +0200

    deleted

--- ex05/kata00.py
+++
@@ -1,6 +0,0 @@
-kata = (19, 42, 21)
-
-if __name__ == "__main__":
-    line = "The {} numbers are: {}".format(
-        len(kata), ', '.join('{}'.format(k) for k in kata))
-    print(line)
--- ex05/kata03.py
+++
@@ -1,5 +0,0 @@
-kata = "The right format"
-
-if __name__ == "__main__":
-    line = kata.rjust(42, '-')
-    print(line, end="")
