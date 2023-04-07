200001f92264973132b27cc02b5e5ffae4cc0f1d
grvelva <grvelva@student.42.fr>
2023-04-07 14:54:08 +0200

    colors added

--- day_01/ex02/test.py
+++ day_01/ex02/test.py
@@ -1,6 +1,28 @@
 from vector import Vector
+from termcolor import colored, cprint
+
 
 if __name__ == '__main__':
+    cprint("Creation tests:", "black", "on_white")
+    print(colored("Bad values in constructor:", "red"))
+    try:
+        vct = Vector([[[[1.]]]])
+    except ValueError:
+        cprint("ValueError", "red")
+    try:
+        vct = Vector(-5)
+    except ValueError:
+        cprint("ValueError", "red")
+    try:
+        vct = Vector(4, 2)
+    except ValueError:
+        cprint("ValueError", "red")
+    except TypeError:
+        cprint("TypeError", "red")
+    try:
+        vct = Vector([[1], [2], [3]])
+    except ValueError:
+        cprint("ValueError", "red")
     vct = Vector([[1.], [2.], [3.]])
     print(vct)
     print(vct.T())
