04b331698e9dca0f474a9a06c2a2036a55c37b1f
grvelva <grvelva@student.42.fr>
2023-04-08 19:14:15 +0200

    added checklist tests

--- day_01/ex03/generator.py
+++ day_01/ex03/generator.py
@@ -1,5 +1,7 @@
 import random
 
+from termcolor import cprint
+
 
 def generator(text, sep=" ", option=None):
     if not isinstance(text, str) or not isinstance(sep, str):
@@ -26,11 +28,36 @@ def generator(text, sep=" ", option=None):
 
 if __name__ == '__main__':
     text = "Le Lorem Ipsum est simplement du faux texte."
+    cprint("Simple test", "green")
     for word in generator(text, sep=" "):
         print(word)
+    cprint("\nShuffle test", "green")
+    for word in generator(text, sep=" ", option="shuffle"):
+        print(word)
+    cprint("\nShuffle test2 (shouldn't be the same)", "green")
     for word in generator(text, sep=" ", option="shuffle"):
         print(word)
 
+    cprint("\nUnique test", "green")
     text = "Lorem Ipsum Lorem Ipsum"
     for word in generator(text, sep=" ", option="unique"):
         print(word)
+
+    cprint("\nCode_blocks tests:", "black", "on_white")
+    txt="This is a simple string for a basic test. Very simple."
+
+    cprint("First test", "green")
+    for elem in generator(txt, sep=' '):
+        print(f"'{elem}'")
+
+    cprint("\nSecond test", "green")
+    for elem in generator(txt, sep='.'):
+        print(f"'{elem}'")
+
+    cprint("\nThird test", "green")
+    for elem in generator(txt, sep='i'):
+        print(f"'{elem}'")
+
+    cprint("\nFourth test", "green")
+    for elem in generator(txt, sep='si'):
+        print(f"'{elem}'")
