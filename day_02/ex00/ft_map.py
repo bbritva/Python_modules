2d3ea37baf7dba3d8cb5656eca4b017eaf4aec95
grvelva <grvelva@student.42.fr>
2023-04-12 15:55:07 +0200

    added ex00

--- day_02/ex00/ft_map.py
+++ day_02/ex00/ft_map.py
@@ -7,4 +7,22 @@ def ft_map(function_to_apply, iterable):
     An iterable.
     None if the iterable can not be used by the function.
     """
-# ... Your code here ...
+    if not hasattr(iterable, "__iter__"):
+        raise TypeError("ft_map() argument #2 must support iteration")
+    if not callable(function_to_apply):
+        raise TypeError("ft_map() argument #1 must be callable")
+    for i in iterable:
+        try:
+            yield function_to_apply(i)
+        except TypeError:
+            yield None
+
+
+if __name__ == "__main__":
+    print(ft_map(lambda x: x + 1, [1, 2, 3, 4, 5]))
+    print(list(ft_map(lambda x: x + 1, [])))
+    print(list(ft_map(lambda x: x + 1, [1])))
+    print(list(ft_map(lambda x: x + 1, [1, 2])))
+    print(list(ft_map(lambda x: x + 1, [1, 2, 3])))
+    print(list(ft_map(lambda x: x + 1, [1, 2, 3, 4])))
+    print(list(ft_map(lambda x: x + 1, [1, 2, 3, 4, 5])))
--- day_02/ex00/ft_reduce.py
+++ day_02/ex00/ft_reduce.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def ft_reduce(function_to_apply, iterable):
     """Apply function of two arguments cumulatively.
     Args:
@@ -7,4 +10,29 @@ def ft_reduce(function_to_apply, iterable):
     A value, of same type of elements in the iterable parameter.
     None if the iterable can not be used by the function.
     """
-# ... Your code here ...
+    if not hasattr(iterable, "__iter__"):
+        raise TypeError("ft_map() argument #2 must support iteration")
+    if not callable(function_to_apply):
+        raise TypeError("ft_map() argument #1 must be callable")
+    try:
+        partial_result = iterable[0]
+        for i in iterable[1:]: # Skip the first element
+            partial_result = function_to_apply(partial_result, i)
+    except TypeError:
+        return None
+    except IndexError:
+        return None
+    return partial_result
+
+if __name__ == "__main__":
+    print(ft_reduce(lambda x, y: x + y, [1, 2, 3, 4, 5]))
+    print(ft_reduce(lambda x, y: x + y, []))
+    print(ft_reduce(lambda x, y: x + y, [1]))
+    print(ft_reduce(lambda x, y: x + y, [1, 2]))
+    print(ft_reduce(lambda x, y: x + y, [1, 2, 3]))
+    print(ft_reduce(lambda x, y: x + y, [1, 2, 3, 4]))
+    print(ft_reduce(lambda x, y: x + y, [1, 2, 3, 4, 5]))
+    lst = ['H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+    print(ft_reduce(lambda u, v: u + v, lst))
+    # Output:
+    "Hello world"
