b9d93d99db13d6005241c3e399a564dbfb7a89bd
Boris <amgboris@ya.ru>
2023-04-25 13:15:39 +0200

    invert works

--- day_03/main.py
+++ day_03/main.py
@@ -6,16 +6,17 @@ from ex03.ColorFilter import ColorFilter
 
 if __name__ == "__main__":
     imp = ImageProcessor()
-    arr = imp.load("assets/42AI.png")
+    arr = imp.load("resources/42AI.png")
     # Output :
     # Loading image of dimensions 200 x 200
-    cf = ColorFilter()
-    cf.invert(arr)
-    print(repr(cf.invert(arr)))
-    imp.display(cf.invert(arr))
-    cf.to_green(arr)
-    cf.to_red(arr)
-    cf.to_blue(arr)
-    cf.to_celluloid(arr)
-    cf.to_grayscale(arr, 'm')
-    cf.to_grayscale(arr, 'weight', weights = [0.2, 0.3, 0.5])
+    if not arr is None:
+        cf = ColorFilter()
+        cf.invert(arr)
+        imp.display(arr)
+        imp.display(cf.invert(arr))
+        cf.to_green(arr)
+        cf.to_red(arr)
+        cf.to_blue(arr)
+        cf.to_celluloid(arr)
+        cf.to_grayscale(arr, 'm')
+        cf.to_grayscale(arr, 'weight', weights = [0.2, 0.3, 0.5])
