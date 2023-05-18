bcf76d85fe652e827c6868e520c7d0f70d75da4d
grvelva <grvelva@student.42.fr>
2023-05-18 21:36:48 +0200

    clustering works, shows result in 3d

--- day_03/ex04/Kmeans.py
+++ day_03/ex04/Kmeans.py
@@ -1,10 +1,31 @@
+import sys
+import numpy as np
+import pandas as pd
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+
 class KmeansClustering:
     def __init__(self, max_iter=20, ncentroid=5):
         self.ncentroid = ncentroid # number of centroids
         self.max_iter = max_iter # number of max iterations to update the centroids
         self.centroids = [] # values of the centroids
 
+    def _guard_(func):
+        def wrapper(*args, **kwargs):
+            if not isinstance(args[1], np.ndarray):
+                print("Not a numpy array", file=sys.stderr)
+                return None
+            try:
+                return(func(*args, **kwargs))
+            except Exception as e:
+                print(e) 
+                return None
+        return wrapper
 
+    @_guard_
     def fit(self, X):
         """
         Run the K-means clustering algorithm.
@@ -21,7 +42,7 @@ class KmeansClustering:
         """
         pass
 
-
+    @_guard_
     def predict(self, X):
         """
         Predict from wich cluster each datapoint belongs to.
@@ -36,3 +57,17 @@ class KmeansClustering:
         This function should not raise any Exception.
         """
         pass
+
+if __name__ == "__main__":
+    raw_data = pd.read_csv('./solar_system_census.csv', index_col = 0)
+
+    model = KMeans(n_clusters=4)
+    print(raw_data)
+    model.fit(raw_data)
+
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+
+    ax.scatter3D(raw_data['height'], raw_data['weight'], raw_data['bone_density'], c=model.labels_)
+    plt.show()
