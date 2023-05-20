b9b0050dc1adb4e9ceb74bccbd4b7efec77e2bbc
Boris <amgboris@ya.ru>
2023-05-20 14:55:01 +0200

    ex04 is done

--- day_04/ex04/SpatioTemporalData.py
+++ day_04/ex04/SpatioTemporalData.py
@@ -8,27 +8,28 @@ class SpatioTemporalData:
     def when(self, location):
         """takes a location as an argument and returns a list containing the
         years where games were held in the given location,"""
-        pass
+        return self.data[self.data["City"] == location]["Year"].unique().tolist()
 
     def where(self, date):
         """takes a date as an argument and returns the location where the
         Olympics took place in the given year."""
-        pass
+        return self.data[self.data["Year"] == date]["City"].unique()
+
 
 if __name__ == "__main__":
     loader = FileLoader()
     data = loader.load('../data/athlete_events.csv')
 
     sp = SpatioTemporalData(data)
-    sp.where(1896)
+    print(sp.where(1896))
     # Output
     # [’Athina’]
-    sp.where(2016)
+    print(sp.where(2016))
     # Output
     # [’Rio de Janeiro’]
-    sp.when('Athina')
+    print(sp.when('Athina'))
     # Output
     # [2004, 1906, 1896]
-    sp.when('Paris')
+    print(sp.when('Paris'))
     # Output
     # [1900, 1924]
