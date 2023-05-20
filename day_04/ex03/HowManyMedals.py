22000d96e27e929e27e7bc527c8d039129a5ac82
Boris <amgboris@ya.ru>
2023-05-20 13:35:57 +0200

    ex03 is done

--- day_04/ex03/HowManyMedals.py
+++ day_04/ex03/HowManyMedals.py
@@ -1,17 +1,23 @@
 from FileLoader import FileLoader
 
 def how_many_medals(data, name):
-    pass
-    # data = data[(data["Year"] == year) & (data["Sex"] == gender)]
-    # all_amount = data["Name"].unique().shape[0]
-    # sport_amount = data[data["Sport"] == sport]["Name"].unique().shape[0]
-    # return sport_amount / all_amount
+    data = data[(data["Name"] == name) & (data["Medal"])]
+    years = data["Year"].unique()
+    result = {}
+    for year in years:
+        medals = data[data["Year"] == year]
+        result[year] = {
+            'G' : medals[medals["Medal"] == "Gold"].shape[0],
+            'S' : medals[medals["Medal"] == "Silver"].shape[0],
+            'B' : medals[medals["Medal"] == "Bronze"].shape[0],
+        }
+    return result
 
 if __name__ == "__main__":
     loader = FileLoader()
     data = loader.load('../data/athlete_events.csv')
 
-    print(how_many_medals(data, "Kjetil Andr Aamodt"))
+    print(repr(how_many_medals(data, "Kjetil Andr Aamodt")))
     # Output
         # {1992: {’G’: 1, ’S’: 0, ’B’: 1},
         #  1994: {’G’: 0, ’S’: 2, ’B’: 1},
