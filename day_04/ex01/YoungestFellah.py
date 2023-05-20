b6e48f4d09374ddc3c3697adc3faefee9fa0e1b4
Boris <amgboris@ya.ru>
2023-05-20 12:35:44 +0200

    ex01 returns dict

--- day_04/ex01/YoungestFellah.py
+++ day_04/ex01/YoungestFellah.py
@@ -1,16 +1,21 @@
 from FileLoader import FileLoader
 
+
 def youngest_fellah(data, year):
     data = data[data["Year"] == year]
     male_age = data[data["Sex"] == "M"]["Age"].min()
     female_age = data[data["Sex"] == "F"]["Age"].min()
-    print("{'f':", female_age,", 'm':", male_age, "}")
+    return {
+        "f": female_age,
+        "m": male_age,
+    }
+
 
 if __name__ == "__main__":
     loader = FileLoader()
     data = loader.load('../data/athlete_events.csv')
     # Output
     # Loading dataset of dimensions 271116 x 15
-    youngest_fellah(data, 2004)
+    print(youngest_fellah(data, 2004))
     # Output
     # {’f’: 13.0, ’m’: 14.0}
