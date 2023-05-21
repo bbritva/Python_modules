2fc865e73e8067f81b7e72c9fe016929ccdcecc7
Boris <amgboris@ya.ru>
2023-05-21 23:34:54 +0200

    added matchig d3_ex04

--- day_03/ex04/Kmeans.py
+++ day_03/ex04/Kmeans.py
@@ -4,25 +4,27 @@ from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
+
 def _guard_(func):
     def wrapper(*args, **kwargs):
         if not isinstance(args[1], np.ndarray):
             print("Not a numpy array", file=sys.stderr)
             return None
         try:
-            return(func(*args, **kwargs))
+            return (func(*args, **kwargs))
         except Exception as e:
-            print(e) 
+            print(e)
             return None
     return wrapper
 
+
 class KmeansClustering:
     def __init__(self, max_iter=20, ncentroid=5):
-        self.ncentroid = ncentroid # number of centroids
-        self.max_iter = max_iter # number of max iterations to update the centroids
-        self.centroids = [] # values of the centroids
-        self.model = KMeans(n_clusters = self.ncentroid, max_iter = self.max_iter)
-
+        self.ncentroid = ncentroid  # number of centroids
+        self.max_iter = max_iter  # number of max iterations to update the centroids
+        self.centroids = []  # values of the centroids
+        self.model = KMeans(n_clusters=self.ncentroid,
+                            max_iter=self.max_iter, n_init=10)
 
     @_guard_
     def fit(self, X):
@@ -58,7 +60,7 @@ class KmeansClustering:
         self.fit(X)
         print(repr(self.model.cluster_centers_))
         return self.model.labels_
-    
+
     @_guard_
     def show(self, X):
         ax = plt.axes(projection='3d')
@@ -66,25 +68,58 @@ class KmeansClustering:
         ax.set_xlabel("height")
         ax.set_ylabel("weight")
         ax.set_zlabel("bone_density")
-        ax.scatter(centers[:,0], centers[:,1], centers[:,2], c=np.arange(self.ncentroid), marker='X', s=150, edgecolor="k")
+        ax.scatter(centers[:, 0], centers[:, 1], centers[:, 2], c=np.arange(
+            self.ncentroid), marker='X', s=150, edgecolor="k")
         if self.ncentroid == 4:
             for i, center in enumerate(self.model.cluster_centers_):
                 amount = sum(self.model.labels_ == i)
-                print("Center #", i, "has coordinates", center, "and", amount, "individuals")
-        ax.scatter3D(X[:,0], X[:,1], X[:,2], c=self.model.labels_)
+                print("Center #", i, "has coordinates",
+                      center, "and", amount, "individuals")
+        ax.scatter3D(X[:, 0], X[:, 1], X[:, 2], c=self.model.labels_)
         plt.show()
+        highest = centers[centers[:, 0].argsort()[::-1]]
+        bonest = centers[centers[:, 2].argsort()]
+        if np.array_equal(highest[0], bonest[0]):
+            belt_center = highest[0]
+            print("Belt center has coordinates", belt_center)
+            data = highest[1:]
+            height_sort = data[data[:, 0].argsort()[::-1]]
+            weight_sort = data[data[:, 1].argsort()[::-1]]
+            if np.array_equal(height_sort[1], weight_sort[1]):
+                if np.array_equal(height_sort[0], weight_sort[2]):
+                    print("Earth center has coordinates", height_sort[1])
+                    print("Venus center has coordinates", height_sort[2])
+                    print("Martian center has coordinates", height_sort[0])
+                else:
+                    print("Can't determine origin")
+            elif np.array_equal(height_sort[1], weight_sort[0]):
+                print("Earth center has coordinates", height_sort[1])
+                print("Venus center has coordinates", height_sort[2])
+                print("Martian center has coordinates", height_sort[0])
+            elif np.array_equal(height_sort[2], weight_sort[1]):
+                print("Earth center has coordinates", weight_sort[1])
+                print("Venus center has coordinates", weight_sort[2])
+                print("Martian center has coordinates", weight_sort[0])
+            else:
+                print("Can't determine origin")
+        else:
+            print("Can't determine origin")
+
+# Belt tallest and has lowest bone density
+# Earth can be on 1 or 2 pos by height
+# Earth can be on 0 or 1 pos by weight
 
 
 def get_param(name, argv):
     for arg in argv:
         if arg.startswith(name + '='):
-            return arg[len(name) + 1 : ]
+            return arg[len(name) + 1:]
     raise ValueError
 
 
 def parse_params(argv):
     try:
-        if len(argv) is not 3:
+        if len(argv) != 3:
             raise ValueError
         params = []
         params.append(get_param('filepath', argv))
