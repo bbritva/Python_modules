0cb2cc1e29717d9f83e100c74db416c1eb594d9d
grvelva <grvelva@student.42.fr>
2023-05-21 13:55:44 +0200

    ex06 started

---
+++ day_04/ex06/MyPlotLib.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+
+
+class MyPlotLib:
+    
+    @staticmethod
+    def histogram(data, features):
+        pass    
+    
+    @staticmethod
+    def density(data, features):
+        pass    
+    
+    @staticmethod
+    def pair_plot(data, features):
+        pass    
+    
+    @staticmethod
+    def box_plot(data, features):
+        pass
