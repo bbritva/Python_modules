df12a901c17f16ebc0e53e8712d6b0030f438b22
grvelva <grvelva@student.42.fr>
2023-05-23 14:49:11 +0200

    ex00 according to code_blocks

--- day_04/ex00/FileLoader.py
+++ day_04/ex00/FileLoader.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import sys
 
 def _guard_(func):
     def wrapper(*args, **kwargs):
         try:
             return(func(*args, **kwargs))
-        except Exception as e:
-            print(e) 
+        except Exception as exc:
+            print(exc, file=sys.stderr)
             return None
     return wrapper
 
@@ -15,6 +16,8 @@ class FileLoader:
         """ takes as an argument the file path of the dataset to load,
         displays a message specifying the dimensions of the dataset (e.g. 340 x 500) and
         returns the dataset loaded as a pandas.DataFrame."""
+        if not isinstance(path, str):
+            raise ValueError("Error: wrong types of args")
         raw_data = pd.read_csv(path)
         print("Loading dataset of dimensions", raw_data.shape[0], "x", raw_data.shape[1])
         return raw_data
@@ -25,9 +28,10 @@ class FileLoader:
         """takes a pandas.DataFrame and an integer as arguments,
         displays the first n rows of the dataset if n is positive, or the last n rows if n is
         negative."""
-        print(df[:n])
-        rows = df.shape[0] if df.shape[0] < n else n
-        print("[", rows, "rows x", df.shape[1], "columns]")
+        if not (isinstance(n, int) and isinstance(df, pd.DataFrame)):
+            raise ValueError("Error: wrong types of args")
+        if n != 0:
+            print(df[:n] if (n > 0) else df[n:])
 
 if __name__ == "__main__":
     fl = FileLoader()
@@ -35,3 +39,7 @@ if __name__ == "__main__":
     fl.display(data, 12)
     data = fl.load("../data/ssc_short.csv")
     fl.display(data, 12)
+    print(type(data))
+    print(isinstance(data, pd.DataFrame))
+    print(isinstance(3, pd.DataFrame))
+    print(isinstance("3", pd.DataFrame))
