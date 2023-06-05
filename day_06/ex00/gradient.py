e7364a18844efb2df0efa55caea3fe2f10e91a5e
Boris <amgboris@ya.ru>
2023-06-05 16:02:09 +0200

    day06 started with ex00

---
+++ day_06/ex00/gradient.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+def simple_gradient(x, y, theta):
+    """Computes a gradient vector from three non-empty numpy.array, with a for-loop.
+    The three arrays must have compatible shapes.
+    Args:
+    x: has to be an numpy.array, a vector of shape m * 1.
+    y: has to be an numpy.array, a vector of shape m * 1.
+    theta: has to be an numpy.array, a 2 * 1 vector.
+    Return:
+    The gradient as a numpy.array, a vector of shape 2 * 1.
+    None if x, y, or theta are empty numpy.array.
+    None if x, y and theta do not have compatible shapes.
+    None if x, y or theta is not of the expected type.
+    Raises:
+    This function should not raise any Exception.
+    """
+    try:
+        # print(x,y,theta)
+        width = x.shape[0]
+        y_hat = np.c_[np.ones(width), x].dot(theta)
+        g_0 = (y_hat - y).sum() / y.shape[0]
+        g_1 = ((y_hat - y) * x).sum() / y.shape[0]
+        return np.array([g_0, g_1])
+    except Exception as e:
+        print(e)
+        return None
+
+x = np.array([12.4956442, 21.5007972, 31.5527382, 48.9145838, 57.5088733]).reshape((-1, 1))
+y = np.array([37.4013816, 36.1473236, 45.7655287, 46.6793434, 59.5585554]).reshape((-1, 1))
+# Example 0:
+theta1 = np.array([2, 0.7]).reshape((-1, 1))
+print(simple_gradient(x, y, theta1))
+# Output:
+# array([[-19.0342574], [-586.66875564]])
+# # Example 1:
+theta2 = np.array([1, -0.4]).reshape((-1, 1))
+print(simple_gradient(x, y, theta2))
+# # Output:
+# array([[-57.86823748], [-2230.12297889]])
