b97cff10cf7090f03dbb61f5229a8f556d62d893
Boris <amgboris@ya.ru>
2023-06-05 18:15:40 +0200

    removed double execution

--- day_06/ex03/my_linear_regression.py
+++ day_06/ex03/my_linear_regression.py
@@ -44,10 +44,10 @@ class MyLinearRegression():
     def loss_elem_(self, y, y_hat):
         return (y - y_hat) ** 2
 
-from my_linear_regression import MyLinearRegression as MyLR
+print("BBBBEEEEGGGGIIINNN")
 x = np.array([[12.4956442], [21.5007972], [31.5527382], [48.9145838], [57.5088733]])
 y = np.array([[37.4013816], [36.1473236], [45.7655287], [46.6793434], [59.5585554]])
-lr1 = MyLR(np.array([[2], [0.7]]))
+lr1 = MyLinearRegression(np.array([[2], [0.7]]))
 # Example 0.0:
 y_hat = lr1.predict_(x)
 print(y_hat)
@@ -70,7 +70,7 @@ print(lr1.loss_(y, y_hat))
 # Output:
 # 195.34539903032385
 # Example 1.0:
-lr2 = MyLR(np.array([[1], [1]]), 5e-8, 1500000)
+lr2 = MyLinearRegression(np.array([[1], [1]]), 5e-8, 1500000)
 lr2.fit_(x, y)
 print(lr2.thetas)
 # Output:
