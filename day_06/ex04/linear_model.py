4c9a45df07341e509acca5f37de13cc83f8ac973
Boris <amgboris@ya.ru>
2023-06-06 12:07:35 +0200

    mse_ works

--- day_06/ex04/linear_model.py
+++ day_06/ex04/linear_model.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+
 
 def _guard_(func):
     def wrapper(*args, **kwargs):
@@ -44,56 +46,23 @@ class MyLinearRegression():
     def loss_elem_(self, y, y_hat):
         return (y - y_hat) ** 2
 
-print("BBBBEEEEGGGGIIINNN")
-x = np.array([[12.4956442], [21.5007972], [31.5527382], [48.9145838], [57.5088733]])
-y = np.array([[37.4013816], [36.1473236], [45.7655287], [46.6793434], [59.5585554]])
-lr1 = MyLinearRegression(np.array([[2], [0.7]]))
-# Example 0.0:
-y_hat = lr1.predict_(x)
-print(y_hat)
-# Output:
-# array([[10.74695094],
-# [17.05055804],
-# [24.08691674],
-# [36.24020866],
-# [42.25621131]])
-# Example 0.1:
-print(lr1.loss_elem_(y, y_hat))
-# Output:
-# array([[710.45867381],
-# [364.68645485],
-# [469.96221651],
-# [108.97553412],
-# [299.37111101]])
-# Example 0.2:
-print(lr1.loss_(y, y_hat))
-# Output:
-# 195.34539903032385
-# Example 1.0:
-lr2 = MyLinearRegression(np.array([[1], [1]]), 5e-8, 1500000)
-lr2.fit_(x, y)
-print(lr2.thetas)
-# Output:
-# array([[1.40709365],
-# [1.1150909 ]])
-# Example 1.1:
-y_hat = lr2.predict_(x)
-print(y_hat)
-# Output:
-# array([[15.3408728 ],
-# [25.38243697],
-# [36.59126492],
-# [55.95130097],
-# [65.53471499]])
-# Example 1.2:
-print(lr2.loss_elem_(y, y_hat))
-# Output:
-# array([[486.66604863],
-# [115.88278416],
-# [ 84.16711596],
-# [ 85.96919719],
-# [ 35.71448348]])
-# Example 1.3:
-print(lr2.loss_(y, y_hat))
-# Output:
-# 80.83996294128525
+    @staticmethod
+    @_guard_
+    def mse_(y, y_hat):
+        return ((y - y_hat) ** 2).sum() / (y.shape[0])
+
+
+try:
+    data = pd.read_csv("are_blue_pills_magic.csv")
+    Xpill = np.array(data["Micrograms"]).reshape(-1, 1)
+    Yscore = np.array(data["Score"]).reshape(-1, 1)
+    linear_model1 = MyLinearRegression(np.array([[89.0], [-8]]))
+    linear_model2 = MyLinearRegression(np.array([[89.0], [-6]]))
+    Y_model1 = linear_model1.predict_(Xpill)
+    Y_model2 = linear_model2.predict_(Xpill)
+    print(MyLinearRegression.mse_(Yscore, Y_model1))
+    # 57.60304285714282
+    print(MyLinearRegression.mse_(Yscore, Y_model2))
+    # 232.16344285714285
+except Exception as e:
+    print("Error:", e)
