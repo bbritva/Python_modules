23005bd42fccc0b1563f5b3627aff495b24a5037
Boris <amgboris@ya.ru>
2023-06-06 17:48:55 +0200

    ex04 done

---
+++ day_07/ex04/fit.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+def _guard_(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return (func(*args, **kwargs))
+        except Exception as e:
+            print(e)
+            return None
+    return wrapper
+
+
+@_guard_
+def predict_(x, theta):
+    return np.c_[np.ones(x.shape[0]), x].dot(theta)
+
+
+@_guard_
+def gradient(x, y, theta):
+    """Computes a gradient vector from three non-empty numpy.array, without any for-loop.
+    The three arrays must have the compatible dimensions.
+    Args:
+    x: has to be an numpy.array, a matrix of dimension m * n.
+    y: has to be an numpy.array, a vector of dimension m * 1.
+    theta: has to be an numpy.array, a vector (n +1) * 1.
+    Return:
+    The gradient as a numpy.array, a vector of dimensions n * 1,
+    containg the result of the formula for all j.
+    None if x, y, or theta are empty numpy.array.
+    None if x, y and theta do not have compatible dimensions.
+    None if x, y or theta is not of expected type.
+    Raises:
+    This function should not raise any Exception.
+    """
+    x_ = np.c_[np.ones(x.shape[0]), x]
+    return x_.T.dot(x_.dot(theta) - y) / y.shape[0]
+
+
+@_guard_
+def fit_(x, y, theta, alpha, max_iter):
+    """
+    Description:
+    Fits the model to the training dataset contained in x and y.
+    Args:
+    x: has to be a numpy.ndarray, a vector of dimension m * 1: (number of training examples, 1).
+    y: has to be a numpy.ndarray, a vector of dimension m * 1: (number of training examples, 1).
+    theta: has to be a numpy.ndarray, a vector of dimension 2 * 1.
+    alpha: has to be a float, the learning rate
+    max_iter: has to be an int, the number of iterations done during the gradient descent
+    Returns:
+    new_theta: numpy.ndarray, a vector of dimension 2 * 1.
+    None if there is a matching dimension problem.
+    Raises:
+    This function should not raise any Exception.
+    """
+    for i in range(max_iter):
+        theta = theta - alpha * gradient(x, y, theta)
+    return theta
+
+
+x = np.array([[0.2, 2., 20.], [0.4, 4., 40.], [0.6, 6., 60.], [0.8, 8., 80.]])
+y = np.array([[19.6], [-2.8], [-25.2], [-47.6]])
+theta = np.array([[42.], [1.], [1.], [1.]])
+# Example 0:
+theta2 = fit_(x, y, theta, alpha = 0.0005, max_iter=42000)
+print(theta2)
+# Output:
+# array([[41.99..],[0.97..], [0.77..], [-1.20..]])
+# Example 1:
+print(predict_(x, theta2))
+# Output:
+# array([[19.5992..], [-2.8003..], [-25.1999..], [-47.5996..]])
