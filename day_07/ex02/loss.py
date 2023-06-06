4140c4d5c387b92cf19e9fd036412326c86beed7
Boris <amgboris@ya.ru>
2023-06-06 17:14:53 +0200

    ex02 done

---
+++ day_07/ex02/loss.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+def _guard_(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return (func(*args, **kwargs))
+        except Exception as e:
+            print(e)
+            return None
+    return wrapper
+
+
+@_guard_
+def loss_(y, y_hat):
+    """Computes the half mean squared error of two non-empty numpy.array, without any for loop.
+    The two arrays must have the same dimensions.
+    Args:
+    y: has to be an numpy.array, a vector.
+    y_hat: has to be an numpy.array, a vector.
+    Returns:
+    The half mean squared error of the two vectors as a float.
+    None if y or y_hat are empty numpy.array.
+    None if y and y_hat does not share the same dimensions.
+    Raises:
+    This function should not raise any Exceptions.
+    """
+    return float((y_hat - y).T.dot((y_hat - y))) / (2 * y.shape[0])
+
+
+X = np.array([0, 15, -9, 7, 12, 3, -21]).reshape((-1, 1))
+Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
+# Example 1:
+print(loss_(X, Y))
+# Output:
+2.142857142857143
+# Example 2:
+print(loss_(X, X))
+# Output
+0.0
