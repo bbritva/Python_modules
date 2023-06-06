b19290f211d6912b255e06afe43b8336af460204
Boris <amgboris@ya.ru>
2023-06-06 16:07:38 +0200

    ex05 ready

--- day_06/ex05/z_score.py
+++ day_06/ex05/z_score.py
@@ -1,4 +1,5 @@
 import numpy as np
+import statistics
 
 
 def _guard_(func):
@@ -11,27 +12,30 @@ def _guard_(func):
     return wrapper
 
 
+@_guard_
 def zscore(x):
     """Computes the normalized version of a non-empty numpy.ndarray using the z-score standardization.
     Args:
     x: has to be an numpy.ndarray, a vector.
     Returns:
-    x’ as a numpy.ndarray.
+    x' as a numpy.ndarray.
     None if x is a non-empty numpy.ndarray or not a numpy.ndarray.
     Raises:
-    This function shouldn’t raise any Exception.
+    This function shouldn't raise any Exception.
     """
-    pass
+    mean = sum(x) / x.size
+    std = (sum([(i - mean) ** 2 for i in x]) / x.size) ** 0.5
+    return ((x - mean) / std).reshape(x.size)
 
 
-X = numpy.array([0, 15, -9, 7, 12, 3, -21])
-zscore(X)
+X = np.array([0, 15, -9, 7, 12, 3, -21])
+print(zscore(X))
 # Output:
-array([-0.08620324, 1.2068453, -0.86203236, 0.51721942, 0.94823559,
-       0.17240647, -1.89647119])
+# array([-0.08620324, 1.2068453, -0.86203236, 0.51721942, 0.94823559,
+#   0.17240647, -1.89647119])
 # Example 2:
 Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
-zscore(Y)
+print(zscore(Y))
 # Output:
-array([0.11267619, 1.16432067, -1.20187941, 0.37558731, 0.98904659,
-       0.28795027, -1.72770165])
+# array([0.11267619, 1.16432067, -1.20187941, 0.37558731, 0.98904659,
+#        0.28795027, -1.72770165])
