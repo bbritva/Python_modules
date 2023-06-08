9365216931ecec1fd50ebf756e5a064418ce281b
Boris <amgboris@ya.ru>
2023-06-08 18:04:56 +0200

    why doesn't it fit enough

--- day_07/ex08/polynomial_train.py
+++ day_07/ex08/polynomial_train.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.arange(1,11).reshape(-1,1)
+x = np.arange(1,11).reshape(-1,1).astype(np.float64)
 y = np.array([[ 1.39270298],
 [ 3.88237651],
 [ 4.37726357],
@@ -11,20 +11,22 @@ y = np.array([[ 1.39270298],
 [ 8.19939795],
 [10.37567392],
 [10.68238222]])
-plt.scatter(x,y)
-plt.grid()
-plt.show()
+# plt.scatter(x,y)
+# plt.grid()
+# plt.show()
 
 from polynomial_model import add_polynomial_features
 from mylinearregression import MyLinearRegression as MyLR
 # Build the model:
 x_ = add_polynomial_features(x, 3)
-my_lr = MyLR(np.ones(4).reshape(-1,1)).fit_(x_, y)
+my_lr = MyLR(np.ones(4).reshape(-1,1), 1e-6, 100000)
+my_lr.fit_(x_, y)
+print(my_lr.thetas)
 # Plot:
 ## To get a smooth curve, we need a lot of data points
 continuous_x = np.arange(1,10.01, 0.01).reshape(-1,1)
 x_ = add_polynomial_features(continuous_x, 3)
-y_hat = my_lr.predict_(continuous_x)
+y_hat = my_lr.predict_(x_)
 plt.scatter(x,y)
 plt.plot(continuous_x, y_hat, color='orange')
 plt.show()
