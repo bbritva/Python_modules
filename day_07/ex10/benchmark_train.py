b984f08081e1b3d13d3588551606af448253fd1c
Boris <amgboris@ya.ru>
2023-06-11 17:25:58 +0200

    all works good for linear uni- and multivariative models

--- day_07/ex10/benchmark_train.py
+++ day_07/ex10/benchmark_train.py
@@ -32,7 +32,7 @@ def univar_processing(data, feature, alpha, thetas=np.array([[0.0], [0.0]]), max
     plot_model(data['x_test'], data['y_test'],
                model_after, model_before, feature)
     print("Thetas:", mlr.thetas)
-    print("MSE = ", mlr.mse_(data['y_test'], model_after))
+    return mlr.mse_(data['y_test'], model_after)
 
 
 def norm_data(x):
@@ -50,25 +50,27 @@ x_train, x_test, y_train, y_test = data_spliter(
     data[["weight", "prod_distance", "time_delivery"]], data[["target"]], 0.8)
 
 max_x = [0.,0.,0.]
+mse = []
 
 for i in range(len(features)):
     x_train[:, i], max_x[i] = norm_data(x_train[:, i])
+    x_test[:,i] /= max_x[i]
 
 for i in range(len(features)):
-    prepared_data = {"x_train": x_train[:, i], "x_test": x_test[:,i] / max_x[i],
+    prepared_data = {"x_train": x_train[:, i], "x_test": x_test[:,i],
             "y_train": y_train, "y_test": y_test, "x_max": max_x[i]}
-    univar_processing(prepared_data, features[i], 1e-1,
-                  thetas=np.array([[6e5], [2e5]]))
+    mse.append(univar_processing(prepared_data, features[i], 1e-1,
+                  thetas=np.array([[6e5], [2e5]])))
 
-exit()
 
 my_lreg = MyLR(thetas=np.array(
-    [[5e5], [3e5], [-2.8e4], [-2e3]]), alpha=1e-1, max_iter=1e5)
+    [[4e5], [3e5], [-3e4], [-2e3]]), alpha=1e-1, max_iter=1e5)
 y_hat_base = my_lreg.predict_(x_test)
 my_lreg.fit_(x_train, y_train)
 y_hat = my_lreg.predict_(x_test)
 print(my_lreg.thetas)
-print("MSE =", my_lreg.mse_(y_test, y_hat))
+mse.append(my_lreg.mse_(y_test, y_hat))
+print(mse)
 
 for i in range(len(features)):
     plot_model(x_test[:,i], y_test, y_hat, y_hat_base, features[i])
