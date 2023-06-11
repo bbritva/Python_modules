db3ed62344c79c1b0557ab8fbb3bb00cef448b02
Boris <amgboris@ya.ru>
2023-06-11 17:54:16 +0200

    univar squared regr works good

--- day_07/ex10/benchmark_train.py
+++ day_07/ex10/benchmark_train.py
@@ -24,7 +24,7 @@ def plot_model(X, Y, Y_hat, Y_had_base, feature):
     plt.show()
 
 
-def univar_processing(data, feature, alpha, thetas=np.array([[0.0], [0.0]]), max_iter=1e5):
+def lin_univar_processing(data, feature, alpha, thetas=np.array([[0.0], [0.0]]), max_iter=1e5):
     mlr = MyLR(thetas, alpha=alpha, max_iter=max_iter)
     model_before = mlr.predict_(data['x_test'])
     mlr.fit_(data['x_train'], data['y_train'])
@@ -35,9 +35,23 @@ def univar_processing(data, feature, alpha, thetas=np.array([[0.0], [0.0]]), max
     return mlr.mse_(data['y_test'], model_after)
 
 
+def sqr_univar_processing(data, feature, alpha, thetas=np.array([[0.0], [0.0], [0.0]]), max_iter=1e5):
+    mlr = MyLR(thetas, alpha=alpha, max_iter=max_iter)
+    x_train_ = add_polynomial_features(data['x_train'], 2)
+    x_test_ = add_polynomial_features(data['x_test'], 2)
+    model_before = mlr.predict_(x_test_)
+    mlr.fit_(x_train_, data['y_train'])
+    model_after = mlr.predict_(x_test_)
+    plot_model(data['x_test'], data['y_test'],
+               model_after, model_before, feature)
+    print("Thetas:", mlr.thetas)
+    return mlr.mse_(data['y_test'], model_after)
+
+
 def norm_data(x):
     x_max = max(x)
     return x / x_max, x_max
+
 
 """ Read data """
 try:
@@ -50,32 +64,37 @@ except FileNotFoundError:
 """ Split and normalize data"""
 x_train, x_test, y_train, y_test = data_spliter(
     data[["weight", "prod_distance", "time_delivery"]], data[["target"]], 0.8)
-max_x = [0.,0.,0.]
-mse_s = []
+max_x = [0., 0., 0.]
+lin_mse = []
 for i in range(len(features)):
     x_train[:, i], max_x[i] = norm_data(x_train[:, i])
-    x_test[:,i] /= max_x[i]
+    x_test[:, i] /= max_x[i]
 
 """ Univariate linear regression """
-for i in range(len(features)):
-    prepared_data = {"x_train": x_train[:, i], "x_test": x_test[:,i],
-            "y_train": y_train, "y_test": y_test, "x_max": max_x[i]}
-    mse_s.append(univar_processing(prepared_data, features[i], 1e-1,
-                  thetas=np.array([[6e5], [2e5]])))
+# for i in range(len(features)):
+#     prepared_data = {"x_train": x_train[:, i], "x_test": x_test[:, i],
+#                      "y_train": y_train, "y_test": y_test, "x_max": max_x[i]}
+#     lin_mse.append(lin_univar_processing(prepared_data, features[i], 1e-1,
+#                                          thetas=np.array([[6e5], [2e5]])))
 
 """ Multivariate linear regression """
-my_lreg = MyLR(thetas=np.array(
-    [[4e5], [3e5], [-3e4], [-2e3]]), alpha=1e-1, max_iter=1e5)
-y_hat_base = my_lreg.predict_(x_test)
-my_lreg.fit_(x_train, y_train)
-y_hat = my_lreg.predict_(x_test)
-print(my_lreg.thetas)
-mse_s.append(my_lreg.mse_(y_test, y_hat))
+# my_lreg = MyLR(thetas=np.array(
+#     [[4e5], [3e5], [-3e4], [-2e3]]), alpha=1e-1, max_iter=1e5)
+# y_hat_base = my_lreg.predict_(x_test)
+# my_lreg.fit_(x_train, y_train)
+# y_hat = my_lreg.predict_(x_test)
+# print(my_lreg.thetas)
+# lin_mse.append(my_lreg.mse_(y_test, y_hat))
+# for i in range(len(features)):
+#     plot_model(x_test[:, i], y_test, y_hat, y_hat_base, features[i])
+
+# for i in range(len(lin_mse)):
+#     print("{:e}".format(lin_mse[i]))
+
+sqr_mse = []
+""" Univariate square regression """
 for i in range(len(features)):
-    plot_model(x_test[:,i], y_test, y_hat, y_hat_base, features[i])
-
-for i in range(len(mse_s)):
-    print("{:e}".format(mse_s[i]))
-
-
-
+    prepared_data = {"x_train": x_train[:, i], "x_test": x_test[:, i],
+                     "y_train": y_train, "y_test": y_test, "x_max": max_x[i]}
+    lin_mse.append(sqr_univar_processing(prepared_data, features[i], 1e-1,
+                                         thetas=np.array([[6e5], [2e5], [2e5]])))
