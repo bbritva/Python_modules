3a735b3b570b0d5f218137ab8ae6c40364eaa505
Boris <amgboris@ya.ru>
2023-06-11 17:41:49 +0200

    added mse output for linear uni- and multivariative models

--- day_07/ex10/benchmark_train.py
+++ day_07/ex10/benchmark_train.py
@@ -39,52 +39,43 @@ def norm_data(x):
     x_max = max(x)
     return x / x_max, x_max
 
-
+""" Read data """
 try:
     data = pd.read_csv("day_07/resources/" + filename)
 except FileNotFoundError:
     data = pd.read_csv("../resources/" + filename)
 except FileNotFoundError:
     exit()
+
+""" Split and normalize data"""
 x_train, x_test, y_train, y_test = data_spliter(
     data[["weight", "prod_distance", "time_delivery"]], data[["target"]], 0.8)
-
 max_x = [0.,0.,0.]
-mse = []
-
+mse_s = []
 for i in range(len(features)):
     x_train[:, i], max_x[i] = norm_data(x_train[:, i])
     x_test[:,i] /= max_x[i]
 
+""" Univariate linear regression """
 for i in range(len(features)):
     prepared_data = {"x_train": x_train[:, i], "x_test": x_test[:,i],
             "y_train": y_train, "y_test": y_test, "x_max": max_x[i]}
-    mse.append(univar_processing(prepared_data, features[i], 1e-1,
+    mse_s.append(univar_processing(prepared_data, features[i], 1e-1,
                   thetas=np.array([[6e5], [2e5]])))
 
-
+""" Multivariate linear regression """
 my_lreg = MyLR(thetas=np.array(
     [[4e5], [3e5], [-3e4], [-2e3]]), alpha=1e-1, max_iter=1e5)
 y_hat_base = my_lreg.predict_(x_test)
 my_lreg.fit_(x_train, y_train)
 y_hat = my_lreg.predict_(x_test)
 print(my_lreg.thetas)
-mse.append(my_lreg.mse_(y_test, y_hat))
-print(mse)
-
+mse_s.append(my_lreg.mse_(y_test, y_hat))
 for i in range(len(features)):
     plot_model(x_test[:,i], y_test, y_hat, y_hat_base, features[i])
 
+for i in range(len(mse_s)):
+    print("{:e}".format(mse_s[i]))
 
-# # theta1 = np.array([[30000.], [4909.], [173.], [6000.]])
-# # theta2 = np.array([[85.], [-7.], [-0.2]])
-# # theta3 = np.array([[87.], [-5.], [-1.5], [0.15]])
-# # theta4 = np.array([[-20], [160], [-80], [14], [-1]])
-# # theta5 = np.array([[1140], [-1850], [1110], [-305], [40], [-2]])
-# # theta6 = np.array([[9110], [-18015], [13400], [-4935], [966], [-96.4], [3.86]])
-# # do_process(theta1, x_train, x_test, y_train, y_test)
-# # do_process(theta2, data)
-# # do_process(theta3, data, 1e-6, 1000000)
-# # do_process(theta4, data, 1e-6, 1000000)
-# # do_process(theta5, data, 1e-8, 1000000)
-# # do_process(theta6, data, 1e-9, 1000000)
+
+
