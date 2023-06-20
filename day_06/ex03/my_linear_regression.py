f62df767f1eddc5af0a87559a3fe7892a1e1969c
Boris <amgboris@ya.ru>
2023-06-20 16:38:17 +0200

    d6 checked

--- day_06/ex03/my_linear_regression.py
+++ day_06/ex03/my_linear_regression.py
@@ -44,7 +44,6 @@ class MyLinearRegression():
     def loss_elem_(self, y, y_hat):
         return (y - y_hat) ** 2
 
-print("BBBBEEEEGGGGIIINNN")
 x = np.array([[12.4956442], [21.5007972], [31.5527382], [48.9145838], [57.5088733]])
 y = np.array([[37.4013816], [36.1473236], [45.7655287], [46.6793434], [59.5585554]])
 lr1 = MyLinearRegression(np.array([[2], [0.7]]))
