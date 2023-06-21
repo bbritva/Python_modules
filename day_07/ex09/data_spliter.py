279c51e52e84d79925ea1f3c831d9b8dab690452
grvelva <grvelva@student.42.fr>
2023-06-21 17:32:12 +0200

    beautified test for data splitter

--- day_07/ex09/data_spliter.py
+++ day_07/ex09/data_spliter.py
@@ -30,13 +30,16 @@ def data_spliter(x, y, proportion):
 if __name__=="__main__":
     x1 = np.array([1, 42, 300, 10, 59]).reshape((-1, 1))
     y = np.array([0, 1, 0, 1, 0]).reshape((-1, 1))
-    # Example 1:
-    print(data_spliter(x1, y, 0.8))
+    print("Example 1:")
+    x_train, x_test, y_train, y_test = data_spliter(x1, y, 0.8)
+    print(np.c_[x_train, y_train])
+    print(np.c_[x_test, y_test])
     # Output:
     # (array([ 1, 59, 42, 300]), array([10]), array([0, 0, 1, 0]), array([1]))
-    # Example 2:
-    print(data_spliter(x1, y, 0.5))
-    # Output:
+    print("Example 2:")
+    x_train, x_test, y_train, y_test = data_spliter(x1, y, 0.5)
+    print(np.c_[x_train, y_train])
+    print(np.c_[x_test, y_test])    # Output:
     # (array([59, 10]), array([ 1, 300, 42]), array([0, 1]), array([0, 0, 1]))
     x2 = np.array([[ 1, 42],
     [300, 10],
@@ -44,8 +47,10 @@ if __name__=="__main__":
     [300, 59],
     [ 10, 42]])
     y = np.array([0, 1, 0, 1, 0]).reshape((-1, 1))
-    # Example 3:
-    print(data_spliter(x2, y, 0.8))
+    print("Example 3:")
+    x_train, x_test, y_train, y_test = data_spliter(x2, y, 0.8)
+    print(np.c_[x_train, y_train])
+    print(np.c_[x_test, y_test])
     # Output:
     # (array([[ 10, 42],
     # [300, 59],
@@ -54,8 +59,10 @@ if __name__=="__main__":
     # array([[ 1, 42]]),
     # array([0, 1, 0, 1]),
     # array([0]))
-    # Example 4:
-    print(data_spliter(x2, y, 0.5))
+    print("Example 4:")
+    x_train, x_test, y_train, y_test = data_spliter(x2, y, 0.5)
+    print(np.c_[x_train, y_train])
+    print(np.c_[x_test, y_test])
     # Output:
     # (array([[59, 1],
     # [10, 42]]),
