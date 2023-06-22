928a0fc6dca36bf8a29b1eb36ec8cdb1fbf7acd5
Boris <amgboris@ya.ru>
2023-06-22 14:07:27 +0200

    ex02 ready

---
+++ day_08/ex02/log_loss.py
@@ -0,0 +1,75 @@
+import numpy as np
+import math
+
+
+def _guard_(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return (func(*args, **kwargs))
+        except Exception as e:
+            print(e)
+            return None
+    return wrapper
+
+
+@_guard_
+def logistic_predict_(x, theta):
+    """Computes the vector of prediction y_hat from two non-empty numpy.ndarray.
+    Args:
+    x: has to be an numpy.ndarray, a vector of dimension m * n.
+    theta: has to be an numpy.ndarray, a vector of dimension (n + 1) * 1.
+    Returns:
+    y_hat as a numpy.ndarray, a vector of dimension m * 1.
+    None if x or theta are empty numpy.ndarray.
+    None if x or theta dimensions are not appropriate.
+    Raises:
+    This function should not raise any Exception.
+    """
+    x_ = np.c_[np.ones((x.shape[0])), x]
+    return 1 / (1 + math.e ** -(x_.dot(theta)))
+
+
+@_guard_
+def log_loss_(y, y_hat, eps=1e-15):
+    """
+    Computes the logistic loss value.
+    Args:
+    y: has to be an numpy.ndarray, a vector of shape m * 1.
+    y_hat: has to be an numpy.ndarray, a vector of shape m * 1.
+    eps: has to be a float, epsilon (default=1e-15)
+    Returns:
+    The logistic loss value as a float.
+    None on any error.
+    Raises:
+    This function should not raise any Exception.
+    """
+    res = 0
+    for i in range(y.shape[0]):
+        res += y[i] * np.log(y_hat[i]) + (1 - y[i]) * np.log(1 - y_hat[i])
+    return float(res / - y.shape[0])
+
+if __name__ == "__main__":
+    # Example 1:
+    y1 = np.array([1]).reshape((-1, 1))
+    x1 = np.array([4]).reshape((-1, 1))
+    theta1 = np.array([[2], [0.5]])
+    y_hat1 = logistic_predict_(x1, theta1)
+    print(log_loss_(y1, y_hat1))
+    # Output:
+    # 0.01814992791780973
+    # Example 2:
+    y2 = np.array([[1], [0], [1], [0], [1]])
+    x2 = np.array([[4], [7.16], [3.2], [9.37], [0.56]])
+    theta2 = np.array([[2], [0.5]])
+    y_hat2 = logistic_predict_(x2, theta2)
+    print(log_loss_(y2, y_hat2))
+    # Output:
+    # 2.4825011602474483
+    # Example 3:
+    y3 = np.array([[0], [1], [1]])
+    x3 = np.array([[0, 2, 3, 4], [2, 4, 5, 5], [1, 3, 2, 7]])
+    theta3 = np.array([[-2.4], [-1.5], [0.3], [-1.4], [0.7]])
+    y_hat3 = logistic_predict_(x3, theta3)
+    print(log_loss_(y3, y_hat3))
+    # Output:
+    # 2.9938533108607053
