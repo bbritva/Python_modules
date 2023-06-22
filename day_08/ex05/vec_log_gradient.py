4212035621c68f76c7efd220e970a7512533fbe4
Boris <amgboris@ya.ru>
2023-06-22 21:43:05 +0200

    ex05 ready, added timer to guard

--- day_08/ex05/vec_log_gradient.py
+++ day_08/ex05/vec_log_gradient.py
@@ -16,26 +16,22 @@ def _guard_(func):
 
 
 @_guard_
-def log_gradient(x, y, theta):
-    """Computes a gradient vector from three non-empty numpy.ndarray, with a for-loop. The three arrays must have compatiblArgs:
+def vec_log_gradient(x, y, theta):
+    """Computes a gradient vector from three non-empty numpy.ndarray, without any for-loop. The three arrays must have compArgs:
     x: has to be an numpy.ndarray, a matrix of shape m * n.
     y: has to be an numpy.ndarray, a vector of shape m * 1.
-    theta: has to be an numpy.ndarray, a vector of shape (n + 1) * 1.
+    theta: has to be an numpy.ndarray, a vector (n +1) * 1.
     Returns:
-    The gradient as a numpy.ndarray, a vector of shape n * 1, containing the result of the formula for all j.
+    The gradient as a numpy.ndarray, a vector of shape n * 1, containg the result of the formula for all j.
     None if x, y, or theta are empty numpy.ndarray.
-    None if x, y and theta do not have compatible dimensions.
+    None if x, y and theta do not have compatible shapes.
     Raises:
     This function should not raise any Exception.
     """
-    dj = np.zeros(theta.shape)
-    x_ = np.c_[np.ones((x.shape[0])), x]
+    x_ = np.c_[np.ones(x.shape[0]), x]
     y_hat = 1 / (1 + math.e ** -(x_.dot(theta)))
-    for i in range(x.shape[0]):
-        dj[0][0] += y_hat[i] - y[i]
-        for j in range(x.shape[1]):
-            dj[j + 1][0] += (y_hat[i][0] - y[i][0]) * (x[i][j])
-    return dj / x.shape[0]
+    return x_.T.dot(y_hat - y) / y.shape[0]
+
 
 
 if __name__ == "__main__":
@@ -43,7 +39,7 @@ if __name__ == "__main__":
     y1 = np.array([1]).reshape((-1, 1))
     x1 = np.array([4]).reshape((-1, 1))
     theta1 = np.array([[2], [0.5]])
-    print(log_gradient(x1, y1, theta1))
+    print(vec_log_gradient(x1, y1, theta1))
     # Output:
     # array([[-0.01798621],
     # [-0.07194484]])
@@ -51,7 +47,7 @@ if __name__ == "__main__":
     y2 = np.array([[1], [0], [1], [0], [1]])
     x2 = np.array([[4], [7.16], [3.2], [9.37], [0.56]])
     theta2 = np.array([[2], [0.5]])
-    print(log_gradient(x2, y2, theta2))
+    print(vec_log_gradient(x2, y2, theta2))
     # Output:
     # array([[0.3715235 ],
     # [3.25647547]])
@@ -59,7 +55,7 @@ if __name__ == "__main__":
     y3 = np.array([[0], [1], [1]])
     x3 = np.array([[0, 2, 3, 4], [2, 4, 5, 5], [1, 3, 2, 7]])
     theta3 = np.array([[-2.4], [-1.5], [0.3], [-1.4], [0.7]])
-    print(log_gradient(x3, y3, theta3))
+    print(vec_log_gradient(x3, y3, theta3))
     # Output:
     # array([[-0.55711039],
     # [-0.90334809],
