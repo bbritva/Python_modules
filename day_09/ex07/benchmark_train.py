f9703056d37ad983f5b27add1bf358cac439b768
grvelva <grvelva@student.42.fr>
2023-07-02 12:12:07 +0200

    added mse plot

--- day_09/ex07/benchmark_train.py
+++ day_09/ex07/benchmark_train.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import pickle
 
 from polynomial_model import add_polynomial_features
@@ -10,14 +11,15 @@ filename = "space_avocado.csv"
 features = ["weight", "prod_distance", "time_delivery"]
 results = {}
 models = {1: {}, 2: {}, 3: {}, 4: {}}
-# max_iter = 1e6
-max_iter = 1e5
+max_iter = 1e6
+# max_iter = 1e2
 alpha = .1
+
 
 def train_model(train_set, power, lambda_):
     theta = np.zeros(((3 * power + 1), 1))
     model = MyLR(theta, alpha, max_iter, lambda_)
-    
+
     x_train_ = add_polynomial_features(train_set[:, :-1], power)
     model.fit_(x_train_, train_set[:, -1].reshape((-1, 1)))
     models[power][lambda_] = model.theta
@@ -31,21 +33,34 @@ def train_models(train_set):
             train_model(train_set, i, j / 5)
 
 
+def plot_mse(power, data):
+    data = data[np.where(data[:,0] == power)]
+    plt.title("Mse with power = " + str(power))
+    plt.plot(data[:, 1], data[:,2], marker='o')
+    plt.grid()
+    plt.show()
+
+
 def validate_models(cv_set):
     best_power = 1
     best_lambda = 0
     best_mse = 0
+    results = []
     for i in range(1, 5):  # powers
         x_cv_ = add_polynomial_features(cv_set[:, :-1], i)
         for j in range(6):  # lambdas
             model = MyLR(models[i][j/5], alpha, max_iter, j/5)
             y_hat = model.predict_(x_cv_)
             mse = MyLR.mse_(cv_set[:, -1].reshape((-1, 1)), y_hat)
+            results.append([i, j / 5, mse])
             if mse < best_mse or best_mse == 0:
                 best_mse = mse
                 best_power = i
                 best_lambda = j / 5
     print(best_power, best_lambda, best_mse)
+    results = np.array(results)
+    for i in range(1, 5):
+        plot_mse(i, results)
     return (best_power, best_lambda, best_mse)
 
 
@@ -66,12 +81,12 @@ if __name__ == "__main__":
             exit()
 
     """ Normalize data"""
-    data = np.array(data[["weight", "prod_distance", "time_delivery", "target"]])
+    data = np.array(
+        data[["weight", "prod_distance", "time_delivery", "target"]])
     max_x = np.zeros((data.shape[1]))
     min_x = np.zeros((data.shape[1]))
     for i in range(data.shape[1]):
-        data[:,i], max_x[i], min_x[i] = norm_data(data[:, i])
-
+        data[:, i], max_x[i], min_x[i] = norm_data(data[:, i])
 
     train_set, cv_set, test_set = data_spliter(data)
     print(train_set.shape, cv_set.shape, test_set.shape)
